7ebf52db2b3abd706b074710231250f5e1b84f13
Suraj S Neelagar <71820711+surajneelagar@users.noreply.github.com>
2025-05-06 05:23:54 -0700

    Add files via upload

---
+++ models.py
@@ -0,0 +1,9 @@
+# from app import db
+# from datetime import datetime
+
+# class BlogPost(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     content = db.Column(db.Text, nullable=False)
+#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
